f2554385fb1e59b8af04902ffa285598159533f0
Jesse Fender <jf0772256@otc.edu>
2016-10-14 11:41:43 -0500

    updated test application.

--- test2.py
+++ test2.py
@@ -4,7 +4,7 @@
     written for the purpose of testing new_dbconnect
     date written: 10/13/2016
     Revised Date: 10/14/2016
-    version: 01.00.10A
+    version: 01.00.11A
 '''
 
 import new_dbconnect as DB
@@ -15,7 +15,9 @@ Employees = DB.Employees()
 Inventory = DB.Inventory()
 Encoder = DB.stringEncode()
 
-PATH = "\\dbfiles\\"
+PATH = "dbfiles\\"
+
+DBControl.readAllDatabaseTables(PATH)
 
 DB.AdminMenu()
 print ""
@@ -23,5 +25,3 @@ DB.Sales_Menu()
 print ""
 DB.MenuItems()
 print ""
-print PATH
-raw_input("press key to exit")
